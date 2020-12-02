2ab8c1cf83a6c5efa3e6ea2995cb0f9494165b36
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-12-02 16:02:48 +0300

    Добавил функции для создания и рендеринга DOM-элементов

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+
 export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
@@ -9,4 +15,23 @@ export const getRandomIntegerNumber = (min, max) => {
 export const getRandomArrayItem = (array) => {
   const randomIndex = getRandomIntegerNumber(0, array.length);
   return array[randomIndex];
+};
+
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+
+export const render = (container, element, place = `beforeend`) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
 };
