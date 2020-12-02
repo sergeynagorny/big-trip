6de41b4cc148ac90539262e5e6eaef54fc09ec7a
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-12-02 16:05:40 +0300

    Обновил импорты и рендеринг элементов

--- src/main.js
+++ src/main.js
@@ -1,42 +1,37 @@
-import {createTripInfoTemplate} from "./view/trip-info.js";
-import {createInfoTitleTemplate} from "./view/info-title.js";
-import {createInfoDatesTemplate} from "./view/info-dates.js";
-import {createTripCostTemplate} from "./view/trip-cost.js";
-import {createControlsMenuTemplate} from "./view/controls-menu.js";
-import {createControlsFilterTemplate} from "./view/controls-filter.js";
-import {createSortTemplate} from "./view/sort.js";
-import {createDaysListTemplate} from "./view/days-list.js";
-import {createDaysItemTemplate} from "./view/days-item.js";
-import {createPointEditTemplate} from "./view/point-edit.js";
+import TripInfoView from "./view/trip-info.js";
+import TripRouteView from "./view/trip-route.js";
+import TripDatesView from "./view/trip-dates.js";
+import TripCostView from "./view/trip-cost.js";
+import ControlsMenuView from "./view/controls-menu.js";
+import ControlsFilterView from "./view/controls-filter.js";
+import SortView from "./view/sort.js";
+import DaysListView from "./view/days-list.js";
+import PointEditView from "./view/point-edit.js";
+
 import {generatePoints} from "./model/point.js";
-import dayjs from 'dayjs';
+import {render} from "./utils.js";
+
 
 const POINTS_COUNT = 15;
 const dataPoints = generatePoints(POINTS_COUNT);
 
-const render = (container, template, place = `beforeEnd`) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 
 const tripInfoContainer = document.querySelector(`.trip-main`);
-render(tripInfoContainer, createTripInfoTemplate(), `afterBegin`);
+render(tripInfoContainer, new TripInfoView().getElement(), `afterbegin`);
 
 const tripInfoMetadataContainer = document.querySelector(`.trip-info__main`);
-render(tripInfoMetadataContainer, createInfoTitleTemplate(dataPoints));
-render(tripInfoMetadataContainer, createInfoDatesTemplate(dataPoints));
+render(tripInfoMetadataContainer, new TripRouteView(dataPoints).getElement());
+render(tripInfoMetadataContainer, new TripDatesView(dataPoints).getElement());
 
 const tripInfoPriceContainer = document.querySelector(`.trip-main__trip-info`);
-render(tripInfoPriceContainer, createTripCostTemplate(dataPoints));
+render(tripInfoPriceContainer, new TripCostView(dataPoints).getElement());
 
 const tripControlsContainer = document.querySelector(`.trip-main__trip-controls`);
-render(tripControlsContainer.querySelector(`h2:nth-of-type(1)`), createControlsMenuTemplate(), `afterEnd`);
-render(tripControlsContainer.querySelector(`h2:nth-of-type(2)`), createControlsFilterTemplate(), `afterEnd`);
-
+render(tripControlsContainer, new ControlsMenuView().getElement());
+render(tripControlsContainer, new ControlsFilterView().getElement());
 
 const tripBoard = document.querySelector(`.trip-events`);
-render(tripBoard, createSortTemplate());
-render(tripBoard, createPointEditTemplate(dataPoints[0]));
-render(tripBoard, createDaysListTemplate());
-const tripDaysList = document.querySelector(`.trip-days`);
-render(tripDaysList, createDaysItemTemplate(dataPoints.slice(1)));
+render(tripBoard, new SortView().getElement());
+render(tripBoard, new PointEditView(dataPoints[0]).getElement());
+render(tripBoard, new DaysListView(dataPoints.slice(1)).getElement());
+
