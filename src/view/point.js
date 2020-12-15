360691596612ab50315a1dfb4da03b8518a6a49f
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-12-15 11:29:29 +0300

    Добавил кнопку `избранный` для точки маршрута

--- src/view/point.js
+++ src/view/point.js
@@ -56,6 +56,8 @@ const createPointTemplate = (point) => {
   const eventDuration = calculateEventDuration(point.date);
   const fullPrice = calculateOffersPrice(point.typeInfo.offers) + point.price;
 
+  const isFavorite = point.isFavorite ? `event__favorite-btn--active` : ``;
+
   const offersMarkup = createOffersMarkup(point.typeInfo.offers);
 
   return (`
@@ -81,6 +83,13 @@ const createPointTemplate = (point) => {
 
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">${offersMarkup}</ul>
+
+        <button class="event__favorite-btn ${isFavorite}" type="button">
+          <span class="visually-hidden">Add to favorite</span>
+          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+          </svg>
+        </button>
 
         <button class="event__rollup-btn" type="button">
           <span class="visually-hidden">Open event</span>
