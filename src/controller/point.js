ffcdd4e49851b75b4f815ddb003d09c68acc17f2
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-12-18 16:22:57 +0300

    Рефакторинг контроллера точек маршрута

--- src/controller/point.js
+++ src/controller/point.js
@@ -3,46 +3,65 @@ import PointEditView from "../view/point-edit.js";
 import {render, replace} from "../utils/render.js";
 
 const renderPoint = (container, point) => {
-  const replacePointToEdit = () => {
-    replace(pointEditView, pointView);
-  };
-
-  const replaceEditToPoint = () => {
-    replace(pointView, pointEditView);
-  };
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      replaceEditToPoint();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const pointView = new PointView(point);
   pointView.setEditButtonClickHandler(() => {
     replacePointToEdit();
     document.addEventListener(`keydown`, onEscKeyDown);
   });
 
-  const pointEditView = new PointEditView(point);
   pointEditView.setSubmitHandler((evt) => {
     evt.preventDefault();
     replaceEditToPoint();
     document.removeEventListener(`keydown`, onEscKeyDown);
   });
-
-  render(container, pointView);
 };
 
 
 export default class PointController {
   constructor(container) {
     this._container = container;
+
+    this._pointView = null;
+    this._pointEditView = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   render(point) {
-    renderPoint(this._container, point)
+    this._pointView = new PointView(point);
+    this._pointEditView = new PointEditView(point);
+
+
+    this._setupEventHandlers();
+    render(this._container, this._pointView);
+  }
+
+  _setupEventHandlers() {
+    this._pointView.setEditButtonClickHandler(() => {
+      this._replacePointToEdit();
+    });
+
+    this._pointEditView.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToPoint();
+    });
+  }
+
+  _replacePointToEdit() {
+    replace(this._pointEditView, this._pointView);
+    document.addEventListener(`keydown`, this._onEscKeyDown);
+  }
+
+  _replaceEditToPoint() {
+    replace(this._pointView, this._pointEditView);
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToPoint();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
   }
 }
