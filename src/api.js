82be4836e2f48b2be87d4a753619a7f0bdd76e87
sergeynagorny <sergey.s.nagorny@gmail.com>
2020-12-30 21:58:13 +0300

    Рефакторинг `api.js`

--- src/api.js
+++ src/api.js
@@ -1,32 +1,68 @@
 import Point from "./model/point.js";
 
+const Method = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 export default class API {
-  constructor(authorization) {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
     this._authorization = authorization;
   }
 
   getPoints() {
-    const headers = new Headers();
-    headers.append(`Authorization`, this._authorization);
-
-    return fetch(`https://11.ecmascript.pages.academy/big-trip/points`, {headers})
+    return (
+      this._load({url: `points`})
       .then((response) => response.json())
-      .then(Point.parsePoints);
+      .then(Point.parsePoints)
+    );
   }
 
   getOffers() {
-    const headers = new Headers();
-    headers.append(`Authorization`, this._authorization);
-
-    return fetch(`https://11.ecmascript.pages.academy/big-trip/offers`, {headers})
-      .then((response) => response.json());
+    return (
+      this._load({url: `offers`})
+        .then((response) => response.json())
+    );
   }
 
   getDestinations() {
-    const headers = new Headers();
+    return (
+      this._load({url: `destinations`})
+        .then((response) => response.json())
+    );
+  }
+
+  updatePoint(id, data) {
+    return (
+      this._load({
+        url: `points/${id}`,
+        method: Method.PUT,
+        body: JSON.stringify(data.toRAW()),
+        headers: new Headers({"Content-Type": `application/json`})
+      })
+        .then((response) => response.json())
+        .then(Point.parsePoint)
+    );
+  }
+
+  _load({url, method = Method.GET, body = null, headers = new Headers()}) {
     headers.append(`Authorization`, this._authorization);
 
-    return fetch(`https://11.ecmascript.pages.academy/big-trip/destinations`, {headers})
-      .then((response) => response.json());
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(checkStatus)
+      .catch((err) => {
+        throw err;
+      });
   }
 }
